17b41c13ceb999b54bcfad7d7d5798bbe24addfb
Ashish kumar Mohanty <ashishkumarmohanty240399@gmail.com>
2022-01-21 20:38:54 +0530

    its done but except that nav bar

---
+++ phonePage.js
@@ -0,0 +1,175 @@
+function listSort(n){
+    var a=document.querySelector("#micromax").value;
+    var res=phones.filter(function(elem){
+        return elem.brand==a;
+    });
+    document.querySelector("#whole").innerHTML="";
+    res.map(function(data){
+    var div=document.createElement("div");
+    var div1=document.createElement("div");
+    div1.setAttribute("class","imgeDiv");
+    var image=document.createElement("img");
+    image.setAttribute("src",data.image);
+    div1.append(image);
+    var name=document.createElement("p");
+    name.setAttribute("class","name");
+    name.textContent=data.name;
+    var price=document.createElement("p");
+    price.setAttribute("class","price");
+    price.textContent=data.price;
+    var shopName=document.createElement("p");
+    shopName.setAttribute("class","shopName");
+    shopName.textContent=data.shopName;
+    var place=document.createElement("p");
+    place.setAttribute("class","place");
+    place.textContent=data.place;
+    var mobile=document.createElement("P");
+    mobile.setAttribute("class","price");
+    mobile.textContent=data.mobile;
+    var button=document.createElement("button");
+    button.setAttribute("class","button");
+    button.textContent="Contact Supplier";
+    div.append(div1,name,price,shopName,place,mobile,button);
+    document.querySelector("#whole").append(div);
+});
+};
+function listSort(n){
+    var a=document.querySelector("#sony").value;
+    var res=phones.filter(function(elem){
+        return elem.brand==a;
+    });
+    document.querySelector("#whole").innerHTML="";
+    res.map(function(data){
+    var div=document.createElement("div");
+    var div1=document.createElement("div");
+    div1.setAttribute("class","imgeDiv");
+    var image=document.createElement("img");
+    image.setAttribute("src",data.image);
+    div1.append(image);
+    var name=document.createElement("p");
+    name.setAttribute("class","name");
+    name.textContent=data.name;
+    var price=document.createElement("p");
+    price.setAttribute("class","price");
+    price.textContent=data.price;
+    var shopName=document.createElement("p");
+    shopName.setAttribute("class","shopName");
+    shopName.textContent=data.shopName;
+    var place=document.createElement("p");
+    place.setAttribute("class","place");
+    place.textContent=data.place;
+    var mobile=document.createElement("P");
+    mobile.setAttribute("class","price");
+    mobile.textContent=data.mobile;
+    var button=document.createElement("button");
+    button.setAttribute("class","button");
+    button.textContent="Contact Supplier";
+    div.append(div1,name,price,shopName,place,mobile,button);
+    document.querySelector("#whole").append(div);
+});
+};
+function listSort(n){
+    var a=document.querySelector("ikal").value;
+    var res=phones.filter(function(elem){
+        return elem.brand==a;
+    });
+    document.querySelector("#whole").innerHTML="";
+    res.map(function(data){
+    var div=document.createElement("div");
+    var div1=document.createElement("div");
+    div1.setAttribute("class","imgeDiv");
+    var image=document.createElement("img");
+    image.setAttribute("src",data.image);
+    div1.append(image);
+    var name=document.createElement("p");
+    name.setAttribute("class","name");
+    name.textContent=data.name;
+    var price=document.createElement("p");
+    price.setAttribute("class","price");
+    price.textContent=data.price;
+    var shopName=document.createElement("p");
+    shopName.setAttribute("class","shopName");
+    shopName.textContent=data.shopName;
+    var place=document.createElement("p");
+    place.setAttribute("class","place");
+    place.textContent=data.place;
+    var mobile=document.createElement("P");
+    mobile.setAttribute("class","price");
+    mobile.textContent=data.mobile;
+    var button=document.createElement("button");
+    button.setAttribute("class","button");
+    button.textContent="Contact Supplier";
+    div.append(div1,name,price,shopName,place,mobile,button);
+    document.querySelector("#whole").append(div);
+});
+};
+function listSort(n){
+    var a=document.querySelector("#oppo").value;
+    var res=phones.filter(function(elem){
+        return elem.brand==a;
+    });
+    document.querySelector("#whole").innerHTML="";
+    res.map(function(data){
+    var div=document.createElement("div");
+    var div1=document.createElement("div");
+    div1.setAttribute("class","imgeDiv");
+    var image=document.createElement("img");
+    image.setAttribute("src",data.image);
+    div1.append(image);
+    var name=document.createElement("p");
+    name.setAttribute("class","name");
+    name.textContent=data.name;
+    var price=document.createElement("p");
+    price.setAttribute("class","price");
+    price.textContent=data.price;
+    var shopName=document.createElement("p");
+    shopName.setAttribute("class","shopName");
+    shopName.textContent=data.shopName;
+    var place=document.createElement("p");
+    place.setAttribute("class","place");
+    place.textContent=data.place;
+    var mobile=document.createElement("P");
+    mobile.setAttribute("class","price");
+    mobile.textContent=data.mobile;
+    var button=document.createElement("button");
+    button.setAttribute("class","button");
+    button.textContent="Contact Supplier";
+    div.append(div1,name,price,shopName,place,mobile,button);
+    document.querySelector("#whole").append(div);
+});
+};
+function listSort(n){
+    var a=document.querySelector("#mi").value;
+    var res=phones.filter(function(elem){
+        return elem.brand==a;
+    });
+    document.querySelector("#whole").innerHTML="";
+    res.map(function(data){
+    var div=document.createElement("div");
+    var div1=document.createElement("div");
+    div1.setAttribute("class","imgeDiv");
+    var image=document.createElement("img");
+    image.setAttribute("src",data.image);
+    div1.append(image);
+    var name=document.createElement("p");
+    name.setAttribute("class","name");
+    name.textContent=data.name;
+    var price=document.createElement("p");
+    price.setAttribute("class","price");
+    price.textContent=data.price;
+    var shopName=document.createElement("p");
+    shopName.setAttribute("class","shopName");
+    shopName.textContent=data.shopName;
+    var place=document.createElement("p");
+    place.setAttribute("class","place");
+    place.textContent=data.place;
+    var mobile=document.createElement("P");
+    mobile.setAttribute("class","price");
+    mobile.textContent=data.mobile;
+    var button=document.createElement("button");
+    button.setAttribute("class","button");
+    button.textContent="Contact Supplier";
+    div.append(div1,name,price,shopName,place,mobile,button);
+    document.querySelector("#whole").append(div);
+});
+}
